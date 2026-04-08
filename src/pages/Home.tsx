71bad4264e28856fc8ef08040a352eb82126b02e
ancuvi <angeloschmidt18@gmail.com>
2026-04-08 07:08:23 +0200

    unsere Arbeit in unsere Räumlichkeiten angepasst

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -255,7 +255,7 @@ const Home = () => {
       </Section>
 
       {/* Showcase / Gallery Section */}
-      <Section title="Unsere Arbeiten" eyebrow="Galerie" background="stone">
+      <Section title="Unsere Räumlichkeiten" eyebrow="Galerie" background="stone">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="group relative aspect-[4/3] overflow-hidden rounded-xl shadow-md cursor-pointer">
             <img
